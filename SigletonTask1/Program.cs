d575b34bee5727e957ce6f672937fd1ee3f7c679
Marcin Tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2024-10-18 08:37:11 +0200

    Initial commit

---
+++ SigletonTask1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace WPTasks;
+
+class Program
+{
+    public class Logger
+    {
+        private static Logger instance;
+        
+        public static Logger Instance()
+        {
+            if (instance is null)
+            {
+                instance = new Logger();
+            }
+            
+            return instance;
+        }
+
+        public void LoggActivity(string log)
+        {
+            
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
