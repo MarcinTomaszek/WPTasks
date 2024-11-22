57ef75fafbfca0f69aeef080238973662f19f8de
marcin.tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2024-11-22 09:09:36 +0100

    state task1

---
+++ StateTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StateTask1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var c = new Context();
+        c.Balance = 100;
+        
+        c.WithdrawCash(100);
+        c.InsertCard();
+        c.WithdrawCash(100);
+        c.InsertPin("1231");
+        c.WithdrawCash(100);
+        c.WithdrawCash(100);
+        c.InsertPin("1233");
+        c.EjectCard();
+    }
+}
