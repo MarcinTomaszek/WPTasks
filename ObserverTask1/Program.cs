b617619e13ad37c0b46ae01db0db91185066d5dd
marcin.tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2024-11-29 08:58:07 +0100

    obsTask

---
+++ ObserverTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ObserverTask1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WeatherStation w1 = new WeatherStation();
+        MobileDisplay md = new MobileDisplay(w1);
+        DesktopDisplay dd = new DesktopDisplay(w1);
+        w1.UpdateMeasurements(22,40,987);
+        md.Display();
+        dd.Display();
+        w1.UnregisterObserver(md);
+        w1.UpdateMeasurements(33,20,999);
+        md.Display();
+        dd.Display();
+    }
+}
